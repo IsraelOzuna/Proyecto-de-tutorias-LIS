3fd49e9ff657ef8e49f337fd0f27c1496dccaf15
IsraelOzuna <iro1904@hotmail.com>
2018-03-14 18:46:51 -0600

    Creacion de proyecto
    Se creo el proyecto en netBeans y se crearon las primeras interfaces gráficas

---
+++ AredEspacio/src/controlador/VentanaMenuDirectorController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author iro19
+ */
+public class VentanaMenuDirectorController implements Initializable {
+
+    @FXML
+    private JFXButton botonGrupos;
+    @FXML
+    private JFXButton botonAlumnos;
+    @FXML
+    private JFXButton botonRecursosReportes;
+    @FXML
+    private JFXButton botonMaestros;
+    @FXML
+    private JFXButton botonRenta;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
