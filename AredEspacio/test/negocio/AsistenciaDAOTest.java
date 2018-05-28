7bffed657aa03b0d38e7b78362f363c3de48422e
Renato <renaswat@gmail.com>
2018-05-28 13:21:39 -0500

    Correcciones

--- AredEspacio/test/negocio/AsistenciaDAOTest.java
+++ AredEspacio/test/negocio/AsistenciaDAOTest.java
@@ -1,5 +1,9 @@
 package negocio;
+import static java.lang.reflect.Array.set;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -8,6 +12,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import persistencia.Asistencia;
+import persistencia.Grupo;
 
 /**
  *
@@ -37,58 +42,38 @@ public class AsistenciaDAOTest {
     }
 
     @Test
-    public void testObtenerAsistenciaFallido() {
+    public void testObtenerAsistenciaExitoso() {
         System.out.println("obtenerAsistencia");
-        Date fecha = null;
-        int idGrupo = 0;
+        persistencia.Alumno nuevoAlumno = new persistencia.Alumno();
+        nuevoAlumno.setIdAlumno(1);
+        Grupo nuevoGrupo = new Grupo();
+        nuevoGrupo.setIdGrupo(2);
+        Date fecha = new GregorianCalendar(2018, Calendar.MAY, 10).getTime();
+        int idGrupo = 2;
+        Asistencia asistenciaComprobar = new Asistencia();
+        asistenciaComprobar.setAsistio("1");
+        asistenciaComprobar.setFecha(fecha);
+        asistenciaComprobar.setIdAlumno(nuevoAlumno);
+        asistenciaComprobar.setIdGrupo(nuevoGrupo);
+        asistenciaComprobar.setIdAsistencia(3);
         AsistenciaDAO instance = new AsistenciaDAO(unidadPersistenciaPruebas);
-        List<Asistencia> expResult = null;
+        List<Asistencia> expResult = new ArrayList<>();
+        expResult.add(asistenciaComprobar);
         List<Asistencia> result = instance.obtenerAsistencia(fecha, idGrupo);
         assertEquals(expResult, result);
     }
 
-
     @Test
-    public void testObtenerListaPorGrupoFallido() {
-        System.out.println("obtenerListaPorGrupo");
-        List<Asistencia> listaPorFecha = null;
-        int idGrupo = 0;
-        AsistenciaDAO instance = new AsistenciaDAO(unidadPersistenciaPruebas);
-        List<Asistencia> expResult = null;
-        List<Asistencia> result = instance.obtenerListaPorGrupo(listaPorFecha, idGrupo);
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of obtenerListaPorAlumno method, of class AsistenciaDAO.
-     */
-    @Test
-    public void testObtenerListaPorAlumnoFallido() {
-        System.out.println("obtenerListaPorAlumno");
-        List<Asistencia> listaPorGrupo = null;
-        int idAlumno = 0;
-        AsistenciaDAO instance = new AsistenciaDAO(unidadPersistenciaPruebas);
-        List<Asistencia> expResult = null;
-        List<Asistencia> result = instance.obtenerListaPorAlumno(listaPorGrupo, idAlumno);
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of RegistrarAsistencia method, of class AsistenciaDAO.
-     */
-    @Test
-    public void testRegistrarAsistencia() {
+    public void testRegistrarAsistenciaExitoso() {
         System.out.println("RegistrarAsistencia");
-        int idAlumno = 100;
-        int idGrupo = 100;
-        Date fecha = null;
-        String asistio = "";
+        int idAlumno = 1;
+        int idGrupo = 2;
+        Date fecha = new Date();
+        String asistio = "1";
         AsistenciaDAO instance = new AsistenciaDAO(unidadPersistenciaPruebas);
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = instance.RegistrarAsistencia(idAlumno, idGrupo, fecha, asistio);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
